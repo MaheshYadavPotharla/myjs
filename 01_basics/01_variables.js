f40ba449641b265286867cc9334a1980aa430756
MaheshYadavPotharla <maheshyadavpotharla10@gmail.com>
2024-11-26 12:54:12 +0000

    conainer config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "Hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+
+// accountId = 2 - re declaration cannot be allowed again
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+console.log("accountId");
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
